3537ea14d32a6981dd739c2423036c04d375c677
Math376i <91528384+Math376i@users.noreply.github.com>
2022-09-26 13:49:07 +0200

    Interfaces setup

--- MovieRatingExample/Application/ReviewService.cs
+++ MovieRatingExample/Application/ReviewService.cs
@@ -1,6 +1,61 @@
-﻿namespace MovieRatingExample.Application;
+﻿using MovieRatingExample.Core.Service;
 
-public class ReviewService
+namespace MovieRatingExample.Application;
+
+public class ReviewService : IReviewService
 {
-    
+    public int GetNumberOffReviewsFromReviwer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetAverageRateFromReviewer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfRatesByReviewer(int reviewer, int rate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfReviews(int movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double GetAverageRateOfMovie(int movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetNumberOfRates(int movie, int rate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetMoviesWithHighestNumberOfTopRates()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetMostProductiveReviewers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetTopRatedMovies(int amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetTopMoviesByReviewer(int reviewer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<int> GetReviewerByMovie(int movie)
+    {
+        throw new NotImplementedException();
+    }
 }
--- MovieRatingExample/Core/Repositories/IReviewRepository.cs
+++ MovieRatingExample/Core/Repositories/IReviewRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace MovieRatingExample.Core.Repositories;
+﻿using MovieRatingExample.Core.Model;
 
+namespace MovieRatingExample.Core.Repositories;
+
+
+/// <summary>
+/// Interface implementere GetAll fra BEReview
+/// </summary>
 public interface IReviewRepository
 {
     
+    BEReview[] GetAll();
 }
